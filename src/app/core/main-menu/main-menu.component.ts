fc4918d1393737c83d17af91e349d53bf1fa9325
thorongil05 <officialantonio@outlook.com>
2025-12-21 11:50:49 +0100

    refactor: set main menu as standalone component

--- src/app/core/main-menu/main-menu.component.ts
+++ src/app/core/main-menu/main-menu.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.scss'],
-  standalone: false,
+  imports: [RouterModule, MatToolbarModule],
 })
 export class MainMenuComponent {}
