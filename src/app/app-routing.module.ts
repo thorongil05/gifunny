73bdb76ccc97a3b145c0fa14db59bac1328ca450
thorongil05 <officialantonio@outlook.com>
2025-12-21 11:47:16 +0100

    refactor: fix problem with grid component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home-view/home/home.component';
+import { FavouritesComponent } from './home-view/favourites/favourites.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'favourites', component: FavouritesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/home-view/grid/grid.component.ts
+++ src/app/home-view/grid/grid.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Gif } from 'src/app/model/gif';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.scss'],
+  imports: [MatGridListModule],
 })
 export class GridComponent {
   @Input() gifs: Gif[] = [];
