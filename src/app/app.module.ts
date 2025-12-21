1728b12ecac3811a490007f2a3e91a134fc66943
thorongil05 <officialantonio@outlook.com>
2025-12-21 11:02:24 +0100

    build: update angular cli to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeViewModule } from './home-view/home-view.module';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HomeViewModule,
-    HttpClientModule,
-    CoreModule,
-    BrowserAnimationsModule // Needed for animations in reactive form
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        HomeViewModule,
+        CoreModule,
+        BrowserAnimationsModule // Needed for animations in reactive form
+    ], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
